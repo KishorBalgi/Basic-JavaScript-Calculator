7b91fe6feeb955521afca78f4310855324cd3261
KishorBalgi <KishorBalgi@users.noreply.github.com>
2021-02-26 21:53:18 +0530

    first commit

---
+++ Resources/JS/script.js
@@ -0,0 +1,154 @@
+'use strict';
+//key selection
+const clr=document.querySelector('.clr');
+const del=document.querySelector('.del');
+const hide=document.querySelector('.hide');
+const add=document.querySelector('.addition');
+const sub=document.querySelector('.subtract');
+const mul=document.querySelector('.multiply');
+const div=document.querySelector('.divide');
+const deci=document.querySelector('.deci');
+const ans=document.querySelector('.ans');
+const zero=document.querySelector('.zero');
+const one=document.querySelector('.one');
+const two=document.querySelector('.two');
+const three=document.querySelector('.three');
+const four=document.querySelector('.four');
+const five=document.querySelector('.five');
+const six=document.querySelector('.six');
+const seven=document.querySelector('.seven');
+const eight=document.querySelector('.eight');
+const nine=document.querySelector('.nine');
+const ansd=document.getElementById('ansd');
+const clcbut=document.querySelector('.calc-icon');
+let display="";
+let answer="";
+let num="";
+let opr=0;
+
+function calc(opr){
+   switch(opr){
+       case 0: answer=parseFloat(num);num="";break; 
+       case 1: answer+=parseFloat(num);num="";opr=5;break;     
+       case 2: answer-=parseFloat(num);num="";opr=5;break;     
+       case 3: answer*=parseFloat(num);num="";opr=5;break;     
+       case 4: answer/=parseFloat(num);num="";opr=5;break;  
+       case 5: break;
+   };
+}
+
+zero.addEventListener('click',function(){
+    ansd.textContent=display+="0";
+    num+="0";
+});
+
+one.addEventListener('click',function(){
+    ansd.textContent=display+="1";
+    num+="1";
+});
+
+two.addEventListener('click',function(){
+    ansd.textContent=display+='2';
+    num+="2";
+});
+
+three.addEventListener('click',function(){
+    ansd.textContent=display+='3';
+    num+="3";
+});
+
+four.addEventListener('click',function(){
+    ansd.textContent=display+='4';
+    num+="4";
+});
+
+five.addEventListener('click',function(){
+    ansd.textContent=display+='5';
+    num+="5";
+});
+
+six.addEventListener('click',function(){
+    ansd.textContent=display+='6';
+    num+="6";
+});
+
+seven.addEventListener('click',function(){
+    ansd.textContent=display+='7';
+    num+="7";
+});
+
+eight.addEventListener('click',function(){
+    ansd.textContent=display+='8';
+    num+="8";
+});
+
+nine.addEventListener('click',function(){
+    ansd.textContent=display+='9';
+    num+="9";
+});
+
+deci.addEventListener('click',function(){
+    ansd.textContent=display+='.';
+    num+=".";
+});
+
+clr.addEventListener('click',function(){
+    ansd.textContent=display='0';
+    answer="";
+    display="";
+    num="";
+    opr=0;
+});
+
+add.addEventListener('click',function(){
+    ansd.textContent=display+='+';
+    calc(opr);
+    opr=1;
+});
+
+sub.addEventListener('click',function(){
+    ansd.textContent=display+='-';    
+    if(opr!=5 && opr!=0)
+        num+="-";
+    else{
+        calc(opr);
+        opr=2;}
+});
+
+mul.addEventListener('click',function(){
+    ansd.textContent=display+='*';
+    calc(opr);
+    opr=3;
+});
+    
+div.addEventListener('click',function(){
+    ansd.textContent=display+='/';
+    calc(opr);
+    console.log(answer);
+    opr=4;
+});
+
+ans.addEventListener('click',function(){
+    calc(opr);
+    ansd.textContent=answer;
+    display=String(answer);
+    opr=5;
+});
+
+hide.addEventListener('click',function(){
+    document.querySelector('.block').classList.add('hidden');
+    document.querySelector('.calc-icon').classList.remove('hidden');
+});
+del.addEventListener('click',function(){
+    display=display.slice(0,-1);
+    num=num.slice(0,-1);
+    ansd.textContent=display;
+});
+
+clcbut.addEventListener('click',function(){
+    document.querySelector('.block').classList.remove('hidden');
+    document.querySelector('.block').classList.add('animate__animated');
+    document.querySelector('.block').classList.add('animate__fadeIn');
+    document.querySelector('.calc-icon').classList.add('hidden');
+});
+
